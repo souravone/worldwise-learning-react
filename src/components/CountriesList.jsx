f6d5a7c7a16ce8f5d5d34a626c27e1e2f50982a1
Sourav Das <souravdas360@gmail.com>
2024-04-14 23:17:46 +0530

    initial commit

---
+++ src/components/CountriesList.jsx
@@ -0,0 +1,27 @@
+import CountryItem from "./CountryItem";
+import styles from "./CountriesList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
+
+function CountriesList() {
+  const { cities, isLoading } = useCities();
+  if (isLoading) return <Spinner />;
+  if (!cities.length)
+    return <Message message="Add your first city in your map" />;
+
+  const countries = cities.reduce((acc, cur) => {
+    if (!acc.map((el) => el.country).includes(cur.country))
+      return [...acc, { country: cur.country, emoji: cur.emoji }];
+    else return acc;
+  }, []);
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.country} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountriesList;
